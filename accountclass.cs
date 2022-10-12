f408375eb6589e00c9ebf228a8bed6ba948490c7
jafarova1 <gultajvj@code.edu.az>
2022-10-11 23:44:27 -0700

    added file

---
+++ accountclass.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region practice
+            #region Array
+            //int[] arr3 = new int[] { 1, 2, 3, 4, 5 };
+
+
+            //foreach (var item in arr1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //int[] nums = new int[5];
+            //nums[0] = 2;
+            //nums[1] = 3;
+            //nums[2] = 17;
+            //Array.Resize(ref nums, 20);
+            //nums[8] = 22;
+            //int[] arr1 = { 1, 2, 3, 4, 5 };
+            //Array.Reverse(arr1);
+            //foreach (var item in arr1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            //    int age = 66;
+            //    Car car3 = new Car(); 
+
+            //    Car car1 = new Car();
+            //    car1.name = "BMW";
+            //    car1.color = "red";
+            //    car1.price = 15000;
+
+            //    Car car2 = new Car();
+            //    car2.name = "Toyota";
+            //    car2.color = "black";
+            //    car2.price = 30000.2;
+
+            //    Console.WriteLine(car1.name + " " + car1.color);
+            //    Console.WriteLine(car2.name + " " + car2.color);
+            //}
+
+            //Employee emp1 = new Employee();
+            //emp1.GetFullName();
+
+            //            Animal animal1 = new Animal("dog");
+
+            //            Console.WriteLine(animal1.name);
+
+
+            //        }
+            //    }
+            //}
+
+            //class Car
+            //{
+            //    public string name;
+            //    public string color;
+            //    public double price;
+
+
+            //}
+
+            //class Animal
+            //{
+            //    public string name;
+
+            //    public Animal(string name)
+            //    {
+            //        Console.WriteLine(name);
+            //        this.name = name;
+            //    }
+            //}
+
+            //class Employee
+            //{
+            //    public string name="Cavid";
+            //    public string surname="bashirov";
+            //    public int age=29;
+
+            //public  void GetFullName()
+            //{
+            //    Console.WriteLine("Cavid Bashirov");
+            //}
+
+
+            //}
+            #endregion
+
+            string username = "Cavid123";
+            string password = "Cavid1993";
+
+            Account account = new Account();
+            account.Login(username,password);
+            
+         
+
+         
+
+
+        }
+    }
+    
+   
+}
+
+
